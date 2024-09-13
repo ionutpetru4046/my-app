de7b5278915d783e1966cd96c4c05f9c47c82006
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-13 12:54:34 +0100

    added framer-motion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,8 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+	// Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
