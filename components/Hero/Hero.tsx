9afd9521780131b7e503de96aca0776d7b34f171
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-13 20:17:38 +0100

    chaged hero text

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,11 +1,5 @@
-import React from 'react'
-
-const Hero = () => {
+export default function Hero() {
   return (
-    <div>
-      <h1 className='from-neutral-100 size-76 text-red-600 text-center flex-col'>hello world</h1>
-    </div>
-  )
+    <div className='text-3xl text-white'>Hero Page</div>
+  );
 }
-
-export default Hero
