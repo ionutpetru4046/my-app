59891c7be24fb204713353afb8b079a87900e5bc
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-14 22:34:36 +0100

    test headings

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
+
 export default function Hero() {
   return (
-      <div className='text-3xl text-white items-center'>Hero Page
-          <h1>hello world</h1>
+      <div className='text-3xl text-red-500 items-center'>Hero Page
+          <h1 className="text-yellow-300">hello world</h1>
+          <h2 className="text-blue-500">welcome to next.js</h2>
       </div>
   );
 }
