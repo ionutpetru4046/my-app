2fb90a902a9c9b107326098f7039fe24be8c2c3c
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-15 10:38:17 +0100

    app-tree properly

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 
 export default function Hero() {
   return (
-      <div className='text-3xl text-red-500 items-center'>Hero Page
-          <h1 className="text-yellow-300">hello world</h1>
-          <h2 className="text-blue-500">welcome to next.js</h2>
-      </div>
+      <div className='text-3xl text-white'>Hero Page</div>
   );
 }
