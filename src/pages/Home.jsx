6b72cd51ebc81c8df16ef3c76d1850846cea82db
ionutpetru4046 <ionut4046@yahoo.com>
2025-06-08 12:15:28 +0100

    background image added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
+import heroImg from '../assets/heroImg.jpeg'
+
 const Home = () => {
-    return (
-      <section className="text-center p-8 bg-gradient-to-r from-blue-500 to-indigo-600 text-white">
+  return (
+    <section
+      className="min-h-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: `url(${heroImg})` }}
+    >
+      <div className="text-white text-center bg-black/50 p-8 rounded">
         <h1 className="text-5xl font-bold mb-4">Explore the World</h1>
-        <p className="text-lg">Start your adventure with Hello.Travelvibesonly.</p>
-      </section>
-    )
-  }
-  
-  export default Home
+        <p className="text-lg">Start your adventure with DreamWorld Travel.</p>
+      </div>npm run dev
+    </section>
+  )
+}
+
+export default Home
