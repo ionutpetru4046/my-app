e63d397ec7b8991920fdcc0c278ff1459eb618b5
ionutpetru4046 <ionut4046@yahoo.com>
2025-06-09 10:33:30 +0100

    logo added to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom'
+import logo from "../assets/travelvibesonly.jpg";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-100 shadow-md px-6 py-4 flex justify-between items-center">
-      <Link to="/" className="text-2xl font-bold text-blue-600">Travelvibesonly</Link>
+      <Link to="/"
+        className="text-2xl font-bold text-blue-600">
+        <img src={logo} alt="Travelvibesonly Logo" className="w-auto h-20 rounded-full" />
+       </Link>
       <div className="space-x-4">
         <Link to="/" className="text-gray-700 hover:text-blue-600 font-bold">Home</Link>
         <Link to="/tours" className="text-gray-700 hover:text-blue-600 font-bold">Tours</Link>
